dabb37494e79983e47e0a20275c0e72bb6a7bc67
Rich Drechsler <65871072+rich-drechsler@users.noreply.github.com>
2025-02-20 10:06:52 -0500

    Add files via upload

--- charclass.sh
+++ charclass.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 #
-# Originally used something like this to build the HEX lists that correspond to
-# various "charater classes" used as arguments in the ByteSelector recursive calls
-# in the bytedump bash script. It was simple script that I didn't save, but when
-# I looked more closely at the hex code definitions of character sets in the bash
-# version (while working on a Java bytedump version) I wasn't convinced that all
-# of them were correct (e.g., lower, upper, punct). However, using this script to
-# generate the various HEX lists reproduces what's hardcoded in the bytedump bash
-# script.
+# I originally used something like this to build the HEX lists that describe which
+# bytes belong to the POSIX "character classes" that can be used on the command line
+# to pick groups of bytes. Those HEX lists are important and they're used internally
+# (by the bash implementation of bytedump) as arguments in many of the ByteSelector
+# recursive calls. I didn't bother saving the original version of this script, so I
+# was forced to rewrite it when I wanted to check those HEX lists again. Since there
+# appears to be a little disagreement about three of those HEX lists, I felt it was
+# important to include this script in the source code, primarily to document how I
+# constructed the HEX lists.
 #
-# Type
+# This all came up because I decided to take a close look at the HEX lists while I
+# was implementing a Java version of the bash ByteSelector function. A few things
+# seemed a little off, so I asked ChatGPT for some help. It took quite a few tries
+# to "frame" the question properly, but eventually ChatGPT gave me decent, but not
+# completely convincing answers about the HEX lists that are currently hardcoded in
+# the bash version of bytedump. A quick look at the actual characters assigned to
+# the first 256 Unicode code points seemed to provide a third, slightly different
+# take on what belongs in the HEX lists that represent the "lower", "upper", and
+# "punct" character classes.
+#
+# If you want to use this script, type
 #
 #     make charclass
 #
-# followed by commands that look like
+# followed by commands that look something like
 #
-#     ./charclass cntrl
 #     ./charclass lower
-#     ./charclass upper     
-#     ./charclass punct     
+#     ./charclass upper
+#     ./charclass punct
+#     ./charclass cntrl
 #
 # to generate the HEX code lists used in various ByteSelector recursive calls that
-# implement "character class" selection. Those lists are duplicated by this script,
-# but some of the entries in a few of the lists may be open to debate. At the very
-# least ChatGPT, when it's asked carefully about them, seems to believe the lower,
-# upper, and punct classes aren't quite right.
+# implement "character class" selection. In each case the output should match the
+# HEX lists that are currently hardcoded in the bash implementation of the bytedump
+# program.
 #
-# NOTE - might need a little investigation, but not urgent right now. This script
-# generates the HEX lists, exactly the way they're currently defined in existing
-# bytedump bash script.
+# NOTE - at this point I can't explain the differences, but I don't think it's an
+# urgent issue, so for now I'm not going to make any changes. This script documents
+# how I built the existing HEX lists, but there definitely are some issues deserve
+# investigation.
 #
 
 matchedbytes=()
@@ -63,7 +74,7 @@ if [[ $1 =~ ^[[:blank:]]*(alnum|alpha|blank|cntrl|digit|graph|lower|print|punct|
 
                 #
                 # Bash can't handle null bytes in strings and command substitution
-                # tosses all trailing newlines, so deal with them separately.
+                # tosses trailing newlines, so deal with them separately.
                 #
                 case "${hex^^}" in
                     00) char="";;
